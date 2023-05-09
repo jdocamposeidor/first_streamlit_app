cec24b39e68b79acf6aa7decf35583fa26539cc8
jdocamposeidor <118555038+jdocamposeidor@users.noreply.github.com>
2023-05-09 08:11:52 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title('Hola mundo!')
+streamlit.title('Restaurant de los viejos')
+streamlit.header('Comida saludable')
+streamlit.text('Pollo a la plancha')
+streamlit.text('Ensalada de tomate y huevo')
+streamlit.text('Lechugita')
