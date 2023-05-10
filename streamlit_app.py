0fb4e0b764bbd37165d0012fefd530d40435cb83
jdocamposeidor <118555038+jdocamposeidor@users.noreply.github.com>
2023-05-10 07:54:29 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,10 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-
-
-#streamlit.text(fruityvice_response.json())
-
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-
-
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -44,4 +38,6 @@ my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fuit list contants:")
 streamlit.dataframe(my_data_rows)
+my_fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', my_fruit_choice)
 
